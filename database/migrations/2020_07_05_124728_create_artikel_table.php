ff9b53764915e468022f4e8a8ba9ed3e856b3d3b
widdanbaehaqi <widdanbaehaqi@mhs.stmik-yadika.ac.id>
2020-07-05 19:51:59 +0700

    make migrations

--- database/migrations/2020_07_05_124728_create_artikel_table.php
+++ database/migrations/2020_07_05_124728_create_artikel_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateArtikelTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('artikel', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('username')->unique();
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('namalengkap')->nullable();
+            $table->string('judul');
+            $table->text('isi');
+            $table->string('slug');
+            $table->string('tag');
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
@@ -30,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('artikel');
     }
 }
